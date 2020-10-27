a0230f6df2d91047123386a6769b119915628193
juho <peruna.kone>
2020-10-27 18:29:53 +0200

    vaihdoin tekstin ja kuvien paikaa

--- weather-app/src/Weather.js
+++ weather-app/src/Weather.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react'
-
+import './Weather.css'
 class Weather extends Component {
   render() {
     return (
       <div className="Weather">
-        <div className="Weather-focused">
-        <div className="Weather_day">ma</div>
+        <div className="Weather_focused">
+          <div className="Weather_day">ma</div>
         <div className="Weather_time">12:00</div>
         <div className="Weather_temp">14°</div>
         <img src='http://openweathermap.org/img/wn/09d.png' alt="Logo" />
         </div>
 
-          <div className="Weather-forecast">
+          <div className="Weather_forecast">
 
         <div className="Weather_box">
           <div className="Weather_day">ma</div>
