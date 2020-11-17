7657eac73bf4120cbccb2736f85a4e42556d521f
juho <peruna.kone>
2020-11-17 18:26:11 +0200

    sus

--- weather-app/src/Weather.js
+++ weather-app/src/Weather.js
@@ -3,46 +3,49 @@ import {getForecast} from './requests'
 import './Weather.css'
 
 class Weather extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      forecasts: null
+    }
+  }
+
   componentDidMount() {
-    getForecast().then(forecast => {
-      console.log(forecast)
+    getForecast().then(forecasts => {
+      this.setState({ forecasts })
     })
   }
 
+  renderWeatherItem(forecast) {
+    return (
+      <div className="Weather_box">
+        <div className="Weather_day">{forecast.dt}</div>
+        <div className="Weather_time">{forecast.dt}</div>
+        <div className="Weather_temp">{forecast.main.temp}</div>
+        <img src={'http://openweathermap.org/img/wn/09d.png'+forecast.weather[0].icon+'.png'} alt="Logo" />
+        </div>
+    )
+  }
+
   render() {
+    const forecasts = this.state.forecasts
+    if(!forecasts) return null
+    console.log(forecasts)
+
+    const focusedWeather = forecasts.list[0]
+
     return (
       <div className="Weather">
         <div className="Weather_focused">
-          <div className="Weather_day">ma</div>
-        <div className="Weather_time">12:00</div>
-        <div className="Weather_temp">14°</div>
+          <div className="Weather_day">{focusedWeather.dt}</div>
+        <div className="Weather_time">{focusedWeather.dt}</div>
+        <div className="Weather_temp">{focusedWeather.main.temp}</div>
         <img src='http://openweathermap.org/img/wn/09d.png' alt="Logo" />
         </div>
 
           <div className="Weather_forecast">
 
-        <div className="Weather_box">
-          <div className="Weather_day">ma</div>
-          <div className="Weather_time">15:00</div>
-          <div className="Weather_temp">16°</div>
-          <img src='http://openweathermap.org/img/wn/09d.png' alt="Logo" />
-          </div>
-
-
-          <div className="Weather_box">
-            <div className="Weather_day">ma</div>
-            <div className="Weather_time">18:00</div>
-            <div className="Weather_temp">18°</div>
-            <img src='http://openweathermap.org/img/wn/09d.png' alt="Logo" />
-            </div>
-
-
-            <div className="Weather_box">
-              <div className="Weather_day">ma</div>
-              <div className="Weather_time">21:00</div>
-              <div className="Weather_temp">14°</div>
-              <img src='http://openweathermap.org/img/wn/09d.png' alt="Logo" />
-              </div>
+          { forecasts.list.map(forecast => this.renderWeatherItem(forecast)) }
 
           </div>
 
