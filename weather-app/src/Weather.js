03f70a525d13ba70dbc8ce587cac1924ca8e2158
juho <peruna.kone>
2020-11-24 18:16:40 +0200

    laitetiin ajat ja päivät ja asteet

--- weather-app/src/Weather.js
+++ weather-app/src/Weather.js
@@ -16,13 +16,30 @@ class Weather extends Component {
     })
   }
 
+  formatDay = timestamp => {
+    const datetime = new Date(timestamp*1000)
+    const day = datetime.getDate()
+    const month = datetime.getMonth() +1
+    return day + '.' + month
+  }
+
+  formatTime = timestamp => {
+    const datetime = new Date(timestamp*1000)
+    const hours = datetime.getHours()
+    let minutes = datetime.getMinutes()
+    if(minutes <10) {
+      minutes = '0' + minutes
+    }
+    return hours + ':' + minutes
+  }
+
   renderWeatherItem(forecast) {
     return (
       <div className="Weather_box">
-        <div className="Weather_day">{forecast.dt}</div>
-        <div className="Weather_time">{forecast.dt}</div>
-        <div className="Weather_temp">{forecast.main.temp}</div>
-        <img src={'http://openweathermap.org/img/wn/09d.png'+forecast.weather[0].icon+'.png'} alt="Logo" />
+        <div className="Weather_day">{this.formatDay(forecast.dt)}</div>
+        <div className="Weather_time">{this.formatTime(forecast.dt)}</div>
+        <div className="Weather_temp">{Math.round(forecast.main.temp)}°</div>
+        <img src={'http://openweathermap.org/img/wn/'+forecast.weather[0].icon+'.png'} alt="Logo" />
         </div>
     )
   }
@@ -37,10 +54,10 @@ class Weather extends Component {
     return (
       <div className="Weather">
         <div className="Weather_focused">
-          <div className="Weather_day">{focusedWeather.dt}</div>
-        <div className="Weather_time">{focusedWeather.dt}</div>
-        <div className="Weather_temp">{focusedWeather.main.temp}</div>
-        <img src='http://openweathermap.org/img/wn/09d.png' alt="Logo" />
+          <div className="Weather_day">{this.formatDay(focusedWeather.dt)}</div>
+        <div className="Weather_time">{this.formatTime(focusedWeather.dt)}</div>
+        <div className="Weather_temp">{focusedWeather.main.temp}°</div>
+        <img src={'http://openweathermap.org/img/wn/' +focusedWeather.weather[0].icon+'.png'} alt="Logo" />
         </div>
 
           <div className="Weather_forecast">
